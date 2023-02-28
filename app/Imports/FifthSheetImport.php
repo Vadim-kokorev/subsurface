95249a3c11cad4e22d296d5e6c428e9c3ed1e6a0
Vadim Kokorev <kokorev-vadim@rambler.ru>
2023-02-28 14:34:17 +0300

    Устранена проблема унификации данных вносимых в таблицу region
    Изменения вносятся всвязи с необходимостью корректного ввода данных в бд.
    А именно входные данные в таблицу "region" должны не повторятся. Это
    достигается путем, использования фасада WithUpserts и функции uniqueBy
    в файле FifthSheetImport.php
     Date:      Tue Feb 28 14:34:17 2023 +0300

--- app/Imports/FifthSheetImport.php
+++ app/Imports/FifthSheetImport.php
@@ -5,14 +5,33 @@ use App\Models\Region;
 use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use App\Imports\NedraImport;
-
-class FifthSheetImport implements ToModel,WithHeadingRow
+use Maatwebsite\Excel\Concerns\WithUpserts;
+class FifthSheetImport implements ToModel, WithHeadingRow, WithUpserts
 {
+
     public function model(array $row)
     {
-            return new Region([
-                'region'    => $row['region'],
-           ]);
-        }  
+            
+        return new Region([
+                'region' => $row['region'],]);
+        }
+    
+        public function uniqueBy()
+        {
+            return 'region';
+        } 
 }
+    /* public function subject(array $row)
+    {
+            $region = Region::where('region', trim($row[2]))->first();
+            
+            if($region && $region->id_region) {
+                return new Subject([
+                    'name'    => $row[0],
+                    'short_name' => $row[1],
+                     'region_id' => $region->id_region,
+               ]);
+            }
+    
+        }       
+}*/
