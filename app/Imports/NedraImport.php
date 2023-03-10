c047a73f52d2408c2752f965f9daf168d2f64742
Vadim Kokorev <kokorev-vadim@rambler.ru>
2023-03-10 11:21:50 +0300

    get the opportunity  importing into two tables with an id relationship yet2.0

--- app/Imports/NedraImport.php
+++ app/Imports/NedraImport.php
@@ -13,51 +13,8 @@ class NedraImport implements WithMultipleSheets
             /*0 => new FirstSheetImport(),
             1 => new SecondSheetImport(),
             2 => new ThirdSheetImport(),
-            3 => new FourthSheetImport(),*/
+            3 => new DepositImport(),*/
             4 => new RegionSubjectImport(),
         ];
     }
 } 
-
-   /* public function subject(array $row)
-    {
-        $region = Region::where('region', trim($row[2]))->first();
-        
-        if($region && $region->id_region) {
-            return new Subject([
-                'name'    => $row[0],
-                'short_name' => $row[1],
-                 'region_id' => $region->id_region,
-           ]);
-        }
-
-    }   
-}
-class SecondSheetImport implements ToModel
-{
-    public function model(array $row)
-    {
-
-    }
-}
-class ThirdSheetImport implements ToModel
-{
-    public function model(array $row)
-    {
-
-    }
-}
-class FourthSheetImport implements ToModel
-{
-    public function model(array $row)
-    {
-
-    }
-}
-class FifthSheetImport implements ToModel
-{
-    public function model(array $row)
-    {
-
-    }
-}*/
