e8cdc46e0791055a38f205a10292f10ae23fb647
Vadim Kokorev <kokorev-vadim@rambler.ru>
2023-03-15 16:12:18 +0300

    import data to table subsurfase_user

--- app/Imports/NedraImport.php
+++ app/Imports/NedraImport.php
@@ -10,9 +10,9 @@ class NedraImport implements WithMultipleSheets
     public function sheets(): array
     {
         return [
-            /*0 => new FirstSheetImport(),
-            2 => new SecondSheetImport(),
-            4 => new RegionSubjectImport(),*/
+            0 => new SubsurfaceImport(),
+            /*2 => new SecondSheetImport(),
+            4 => new RegionSubjectImport(),
             3 => new DepositImport(),
             /*1 => new LicenseAreaImport(),*/
         ];
