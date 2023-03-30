34504a21ce92b4418d31a394d4a9d403f8a514d3
Vadim Kokorev <kokorev-vadim@rambler.ru>
2023-03-30 15:17:15 +0300

    import ID of license_area and deposit in deposit_area

--- app/Imports/DepositImport.php
+++ app/Imports/DepositImport.php
@@ -13,20 +13,30 @@ class DepositImport implements WithHeadingRow, ToModel/*, WithValidation*/
     protected $deposit;
     public $subject;
     public $area;
-  
+
     public function model(array $row)
     {
+        $deposit = $this->deposit = DB::table('deposit')
+        ->select('deposit', 'id_deposit')
+        ->where('deposit', Str::before($row['deposit'], "("))->first();
         $reg = trim($row['license_area']);
         $regex = "/(.*?) \((.*)/";
         $reg = preg_replace($regex, "$1", $reg);
         $strarea = array_values(array_unique(explode("\n", $reg)));
-        for ($i = 0; $i < count($strarea); $i++){
+        for ($i = 0; $i < count($strarea); $i++) {
 
-        DB::table('license_area')->upsert([
-            ['area' => $strarea[$i]],
-        ], ['area']);
-    }
-        /*DB::table('deposit')->upsert(
+            DB::table('license_area')->upsert([
+                ['area' => $strarea[$i]],
+            ], ['area']);
+            $vararea = $this->area = DB::table('license_area')
+            ->select('id_area', 'area')
+            ->where('area', 'like', $strarea[$i].'%')->first();
+        
+            DB::table('deposit_area')->insert([
+                ['area_id' => $vararea->id_area, 'deposit_id' => $deposit->id_deposit]
+            ]);
+        }
+        DB::table('deposit')->upsert(
             ['deposit' => Str::before($row['deposit'], "("), 'development' => trim($row['development'])],
             ['deposit']
         );
@@ -37,23 +47,9 @@ class DepositImport implements WithHeadingRow, ToModel/*, WithValidation*/
         $deposit = $this->deposit = DB::table('deposit')
             ->select('deposit', 'id_deposit')
             ->where('deposit', Str::before($row['deposit'], "("))->first();
-        $reg = trim($row['license_area']);
-        $regex = "/(.*?) \((.*)/";
-        $reg = preg_replace($regex, "$1", $reg);
-        $strarea = array_values(array_unique(explode("\n", $reg)));
-        var_dump($strarea);
-        for ($i = 0; $i < count($strarea); $i++){
-            $vararea = $this->area = DB::table('license_area')
-            ->select('id_area', 'area')
-            ->where('area', 'like', $strarea[$i].'%')->first();
-        
-            DB::table('deposit_area')->insert([
-                ['area_id' => $vararea->id_area, 'deposit_id' => $deposit->id_deposit]
-            ]);
-        }
     
         DB::table('subject_deposit')->insert([
             ['deposit_id' => $deposit->id_deposit, 'subject_id' => $subject->id_subject]
-        ]);*/
+        ]);
     }
 }
